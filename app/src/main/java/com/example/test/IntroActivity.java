fa64b80e9630de0c5d3c82fa70f089ab751b568f
Jyeo <dhkfkr134@naver.com>
2020-10-01 22:11:27 +0900

    .

---
+++ app/src/main/java/com/example/test/IntroActivity.java
@@ -0,0 +1,28 @@
+package com.example.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Handler x = new Handler();
+        x.postDelayed(new SplashHandler(),5000);
+
+    }
+    public class SplashHandler implements Runnable{
+        public void run(){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+}
