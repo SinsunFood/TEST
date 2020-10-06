a13246f320cebb3210c7b1cf8ad46d5a35fb3059
siwookim97 <tldn12gh@gmail.com>
2020-10-06 18:00:33 +0900

    click button brench

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+        public void sendMessage1(View view){ // button1로의 반찬가게 이동
+            Intent intent = new Intent(getApplicationContext(), MainActivityButton1.class);
+            startActivity(intent);
+        }
+
+        public void sendMessage2(View view){ // button2로의 반찬가게 이동
+            Intent intent = new Intent(getApplicationContext(), MainActivityButton2.class);
+            startActivity(intent);
+        }
+
+        public void sendMessage3(View view){ // button3로의 반찬가게 이동
+            Intent intent = new Intent(getApplicationContext(), MainActivityButton3.class);
+            startActivity(intent);
+        }
 }
