52ca91a927560105cb373f7d4b1f9ab2ac10d054
Cheolsoon <mn5724@naver.com>
2020-11-04 00:19:16 +0900

    cart

---
+++ app/src/main/java/com/example/test/Data.java
@@ -0,0 +1,23 @@
+package com.example.test;
+
+public class Data {
+
+    int Category;
+    String Title;
+    String Content;
+
+    public Data(int category, String title, String content) {
+        Category = category;
+        Title = title;
+        Content = content;
+    }
+
+    public int getCategory() {
+        return Category;
+    }
+
+    public void setCategory(int category) {
+        
+    }
+
+}
