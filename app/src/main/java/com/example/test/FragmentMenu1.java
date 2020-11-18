234d0370ac437fc13ff2557b35f82bd4d21f00fb
Jyeo <dhkfkr134@naver.com>
2020-11-18 23:00:40 +0900

    .

--- app/src/main/java/com/example/test/FragmentMenu1.java
+++ app/src/main/java/com/example/test/FragmentMenu1.java
@@ -66,7 +66,7 @@ public class FragmentMenu1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu2, container, false);
+        return inflater.inflate(R.layout.fragment_menu1, container, false);
     }
 /*
     public get*/
